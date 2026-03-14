5b03f350875d0c9c639bc61d2831fe614e837490
bububuger <guiqiangwhu@gmail.com>
2026-03-14 22:50:47 +0800

    test(BUB-89): relax stalled retry timing assertion

--- elixir/test/symphony_elixir/orchestrator_status_test.exs
+++ elixir/test/symphony_elixir/orchestrator_status_test.exs
@@ -1254,7 +1254,7 @@ defmodule SymphonyElixir.OrchestratorStatusTest do
 
     assert is_integer(due_at_ms)
     remaining_ms = due_at_ms - System.monotonic_time(:millisecond)
-    assert remaining_ms >= 8_500
+    assert remaining_ms >= 7_500
     assert remaining_ms <= 10_500
 
     retry_entry = Enum.find(snapshot.retrying, fn entry -> entry.issue_id == issue_id end)
